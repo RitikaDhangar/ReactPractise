1e1887e80b209bab936e7105750bae279198f6c7
Ritika Dhangar <Ritika Dhangar>
2025-01-10 16:56:14 +0530

    add code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,25 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 const Home = () => {
-  const [val, setVal] = useState("");
-  const [msg, setMsg] = useState("");
-  const submitHandler = async () => {
-    try {
-      const userName = localStorage.getItem("token");
-      console.log({ msg });
-      await axios.post("http://localhost:8000/Store-message", {
-        msg: `${userName} : ${val}`,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  const showMessagesHandler = async () => {
-    try {
-      const data = await axios.get("http://localhost:8000/show-message");
-      console.log({ data });
-      if (data?.status === 200) {
-        setMsg(data?.data?.data);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    showMessagesHandler();
-  }, []);
+  const [product, setProduct] = useState({ title: '', price: '', imageurl: '', description: "" });
+  
+  const onChangeHandler = (e) => {
+    setProduct((prev)=> ({...prev, [e.target.name]:e.target.value}))
+  }
+
+  const submitHandler = () => {
+    
+  }
   return (
     <>
-      <p>{msg}</p>
-      <input type="text" value={val} onChange={(e) => setVal(e.target.value)} />
-      <button onClick={submitHandler}>Add Message</button>
+      <input type="text" placeholder="product Title" value={product.title} onChange={onChangeHandler} name={"title"} />
+      <br/>
+      <input type="text" placeholder="product Price" value={product.price} onChange={onChangeHandler} name={"price"}/>
+      <br/>
+      <input type="text" placeholder="ImageURL" value={product.imageurl} onChange={onChangeHandler} name={"imageurl"}/>
+      <br/>
+      <input type="text" placeholder="description"  value={product.description} onChange={onChangeHandler} name={"title"}/>
+      <button onClick={submitHandler} >Submit product</button>
     </>
   );
 };
