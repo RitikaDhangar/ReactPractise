7365384957ec0f2fc2a6eef858f42155ccb94ef2
dhangarritika <dhangarritika90@gmail.com>
2025-01-12 20:49:07 +0530

    add code

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// If you need to measure performance, uncomment and use the function below:
+// import reportWebVitals from "./reportWebVitals";
+// reportWebVitals(console.log); // or send to an analytics endpoint
