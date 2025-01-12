2719ef3f6df33b47bdc88d87ba57063f1c751216
dhangarritika <dhangarritika90@gmail.com>
2025-01-12 11:58:29 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Home from "./components/Home";
 import Datapage from "./components/Datapage";
+import { useState } from "react";
 
 function App() {
-
+  const [product, setProduct] = useState({
+    title: "",
+    price: "",
+    imageUrl: "",
+    description: "",
+    id: "",
+  });
   return (
     <>
-      <Home />
-      <Datapage/>
+      <Home product={product} setProduct={setProduct} />
+      <Datapage setProductObj={setProduct} />
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,65 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-const Home = () => {
-  const [product, setProduct] = useState({ title: '', price: '', imageurl: '', description: "" });
-  
+const Home = ({ product, setProduct }) => {
   const onChangeHandler = (e) => {
-    setProduct((prev)=> ({...prev, [e.target.name]:e.target.value}))
-  }
+    setProduct((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const submitHandler = async() => {
+  const submitHandler = async () => {
     try {
-      const res = await axios.post('http://localhost:8000/add-items', product);
+      const res = await axios.post("http://localhost:8000/add-items", product);
       if (res?.data?.success === 1) {
         console.log(res?.data?.message);
       }
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+  const editHandler = async () => {
+    try {
+      const res = await axios.put("http://localhost:8000/update-item", product);
+      if (res?.data?.success === 1) {
+        console.log("Edit product successfully");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-      <input type="text" placeholder="product Title" value={product.title} onChange={onChangeHandler} name={"title"} />
-      <br/>
-      <input type="text" placeholder="product Price" value={product.price} onChange={onChangeHandler} name={"price"}/>
-      <br/>
-      <input type="text" placeholder="ImageURL" value={product.imageurl} onChange={onChangeHandler} name={"imageurl"}/>
-      <br/>
-      <input type="text" placeholder="description"  value={product.description} onChange={onChangeHandler} name={"description"}/>
-      <button onClick={submitHandler} >Submit product</button>
+      <input
+        type="text"
+        placeholder="product Title"
+        value={product.title}
+        onChange={onChangeHandler}
+        name={"title"}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="product Price"
+        value={product.price}
+        onChange={onChangeHandler}
+        name={"price"}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="ImageURL"
+        value={product.imageUrl}
+        onChange={onChangeHandler}
+        name={"imageUrl"}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="description"
+        value={product.description}
+        onChange={onChangeHandler}
+        name={"description"}
+      />
+      <button onClick={submitHandler}>Submit product</button>
+      <button onClick={editHandler}>Edit product</button>
     </>
   );
 };
